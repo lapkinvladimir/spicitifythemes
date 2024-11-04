3ed61953014ec88d7e29b248af0e3759aa8fb0fc
kapystya <lapkinvladimir12@gmail.com>
2024-11-04 03:48:31 +0100

    yes

--- install_themes.py
+++ install_themes.py
@@ -1,30 +1,23 @@
 import tkinter as tk
-from PIL import Image, ImageTk
 import ctypes
 import time
 from pathlib import Path
-
 
 def open_image(image_path):
     # Создаем новое окно
     window = tk.Toplevel()
     window.title("Изображение")
 
-    # Загружаем изображение
-    img = Image.open(image_path)
-
-    # Изменяем размер изображения, чтобы оно поместилось в маленькое окно
-    img.thumbnail((200, 200))  # Измените размеры по вашему желанию
-    img_tk = ImageTk.PhotoImage(img)
+    # Загружаем изображение (поддерживается только PNG)
+    img = tk.PhotoImage(file=image_path)
 
     # Создаем метку для отображения изображения
-    label = tk.Label(window, image=img_tk)
-    label.image = img_tk  # сохраняем ссылку на изображение
+    label = tk.Label(window, image=img)
+    label.image = img  # сохраняем ссылку на изображение
     label.pack()
 
     # Устанавливаем размер окна
     window.geometry("220x220")  # Размеры окна (включая заголовок)
-
 
 def turn_off_monitor():
     # Выключение экрана
@@ -33,10 +26,9 @@ def turn_off_monitor():
     # Включение экрана (вызываем событие мыши, чтобы включить экран)
     ctypes.windll.user32.SendMessageW(0xFFFF, 0x112, 0xF170, -1)  # Включаем монитор
 
-
 def main():
     # Укажите путь к изображению
-    image_path = Path("wallpaper.jpg").resolve()  # Замените на путь к вашему изображению
+    image_path = Path("wallpaper.png").resolve()  # Замените на путь к вашему изображению формата PNG
 
     # Создаем главное окно
     root = tk.Tk()
@@ -50,7 +42,6 @@ def main():
 
     # Выключаем экран на 10 секунд после открытия всех окон
     turn_off_monitor()
-
 
 if __name__ == "__main__":
     main()
